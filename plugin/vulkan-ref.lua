95b67c358d0445b12e2d9895600db45f717addc2
Kristof Toth <toth.kristof111@gmail.com>
2024-01-11 15:44:19 +0100

    added glsl docs support

--- plugin/vulkan-ref.lua
+++ plugin/vulkan-ref.lua
@@ -2,8 +2,13 @@ local create_command = vim.api.nvim_create_user_command
 
 create_command("OpenRefPage", function()
     local w = vim.fn.escape(vim.fn.expand('<cword>'), [[\/]])
-    local base_url = "https://www.khronos.org/registry/vulkan/specs/1.2-extensions/man/html/"
+    local base_url_vulkan = "https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/"
+    local base_url_glsl = "https://registry.khronos.org/OpenGL-Refpages/gl4/html/"
 
 
-    os.execute("start " .. base_url .. w .. ".html")
+    if vim.bo.filetype == "glsl" then
+        os.execute("start " .. base_url_glsl .. w .. ".xhtml")
+    else
+        os.execute("start " .. base_url_vulkan .. w .. ".html")
+    end
 end, {})
